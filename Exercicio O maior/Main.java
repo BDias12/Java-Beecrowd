8dfcba0cb23c1ec41ab53eafddce0aede844dcee
BDias12 <brunodc.comercial@gmail.com>
2024-11-07 16:35:35 -0300

    beecrowd | 1013

---
+++ Exercicio O maior/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import static java.lang.Math.abs;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        Scanner sc = new Scanner(System.in);
+        int a, b, c;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+
+
+        int MaiorAB = (a + b + abs(a-b))/2;
+
+        int maior = (MaiorAB + c + abs(MaiorAB - c))/2;
+
+        System.out.printf("%d eh o maior\n", maior);
+    }
+}
